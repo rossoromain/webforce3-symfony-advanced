89f4d43d660ba0b10425b75b90ca32c713c5689a
Romain ROSSO <bif.rosso@gmail.com>
2022-06-07 11:12:58 +0200

    lundi/pause-matin

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    #[Route('/home', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('blog/home.html.twig', [
+            'title' => 'Bienvenue sur le blog',
+            'age' => '16',
+        ]);
+    }
+
+    #[Route('/blog', name: 'blog')]
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+}
